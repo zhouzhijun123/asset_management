38739389c2ed10b59b5ad0bd74532e669700010a
zhouzhijun123 <44854316+zhouzhijun123@users.noreply.github.com>
2020-11-06 09:37:07 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+     <el-button class="download" type="primary">默认按钮</el-button>
+     <i class="el-icon-edit"></i>
   </div>
 </template>
